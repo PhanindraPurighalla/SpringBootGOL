799b6bee58a5b9cb9d38d5157d2c530c5b315bb4
Phanindra Purighalla <phanindra.purighalla@gmail.com>
2021-08-14 23:06:12 +0530

    1. Removed redundant code 2. Added user interface - landing.html and games.html

--- game-of-life/src/main/java/com/aarnasolutions/learning/gameoflife/data/entity/Player.java
+++ game-of-life/src/main/java/com/aarnasolutions/learning/gameoflife/data/entity/Player.java
@@ -10,29 +10,23 @@ import javax.persistence.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Embeddable
-@Table(name="PLAYER")
+@Table(name = "PLAYER")
 public class Player {
-    /*@Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="PLAYER_ID")
-    private long playerId;*/
+  @Column(name = "PLAYER_NAME")
+  private String playerName;
 
-    @Column(name="PLAYER_NAME")
-    private String playerName;
+  @Column(name = "CELL_POSITION")
+  private long cellPosition;
 
-    @Column(name="CELL_POSITION")
-    private long cellPosition;
+  @Column(name = "CAREER_ID")
+  private long careerId;
 
-    @Column(name="CAREER_ID")
-    private long careerId;
+  @Column(name = "CASH")
+  private long cash;
 
-    @Column(name="CASH")
-    private long cash;
+  @Column(name = "BANK_LOAN")
+  private long bankLoan;
 
-    @Column(name="BANK_LOAN")
-    private long bankLoan;
-
-    @Column(name="NUMBER_SPUN")
-    private int numberSpun;
-
+  @Column(name = "NUMBER_SPUN")
+  private int numberSpun;
 }
--- game-of-life/src/main/java/com/aarnasolutions/learning/gameoflife/web/GameOfLifeWebController.java
+++ game-of-life/src/main/java/com/aarnasolutions/learning/gameoflife/web/GameOfLifeWebController.java
@@ -1,30 +1,42 @@
 package com.aarnasolutions.learning.gameoflife.web;
 
-import com.aarnasolutions.learning.gameoflife.business.domain.PlayerTurn;
 import com.aarnasolutions.learning.gameoflife.business.service.ActionService;
-import com.aarnasolutions.learning.gameoflife.business.service.PlayerTurnService;
+import com.aarnasolutions.learning.gameoflife.business.service.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/gol-actions")
-public class ActionWebController {
+@RequestMapping("/game-of-life")
+public class GameOfLifeWebController {
 
     private final ActionService actionService;
+    private final GameService gameService;
 
     @Autowired
-    public ActionWebController(ActionService actionService) {
+    public GameOfLifeWebController(ActionService actionService, GameService gameService) {
         this.actionService = actionService;
+        this.gameService = gameService;
     }
 
-    @GetMapping
+    @GetMapping("/actions")
     public String getActions(Model model) {
         var actions = this.actionService.getActions();
         model.addAttribute("actions", actions);
         return "actions";
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "landing";
+    }
+
+    @GetMapping("/games")
+    public String getGames(Model model) {
+        var games = this.gameService.getGames();
+        model.addAttribute("games", games);
+        return "games";
     }
 }
